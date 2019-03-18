010dc9a0edb9f8eb245ac66f70e6ab452d62b476
Nekapital <nekapital@yandex.com>
2019-03-19 01:53:13 +0300

    root-page changed

--- resources/views/dbco/root.blade.php
+++ resources/views/dbco/root.blade.php
@@ -7,18 +7,49 @@
 	</div>
 	
 	
-	<div class="container-fluid offer3 mt-5">
+	
+	
+	<div class="container-fluid offer4 mt-5">
 		<div class="container" style="height:100%">
 			<div class="row" style="height:100%">
-				<div class="col-12 offset-lg-6 col-lg-6 offer3_right pl-4">
+			
+				<div class="col-6 d-none d-lg-block offer4_img_block">
+					
+				</div>
+				
+				<div class="col-12 col-lg-6 offer4_right pl-5 pb-4">
 					<h3 class="my-4">Всё для построения именно вашего решения</h3>
 					<p>Не дайте потеряться нужным разрозненным сведениям, которые не удается сохранить в стандартной учетной системе. Платформа dbco поможет построить именно то, что необходимо вам и вашему бизнесу. Вы лучше других понимаете все потребности и особенности.</p> 
 					<p>Просто выберите и включите готовые решения и дополнительные функции – через минуту все готово к использованию.</p>
-					<a href="#" role="button" class="btn btn-secondary but1 px-4">Попробовать прямо сейчас</a>
+					<a href="{{ route('resources.main') }}" role="button" class="btn btn-secondary but1 px-4">Попробовать прямо сейчас</a>
 				</div>
+				
 			</div>
 		</div>
 	</div>
+	
+	
+	<div class="container-fluid offer4 mt-5">
+		<div class="container" style="height:100%">
+			<div class="row" style="height:100%">
+			
+				
+				
+				<div class="col-12 col-lg-6 offer4_right pr-5 pb-4">
+					<h3 class="my-4">Что такое решение dbco</h3>
+					<p>Пользовательские решения — это приложение на рабочем столе, включающее все решения, которые пользователь загрузил, создал или адаптировал под свои нужды с помощью платформы dbco.</p> 
+					<p>Готовые решения умеют управлять контактами, отслеживать продажи и запасы, планировать мероприятия и многое другое. Специально созданные решения автоматизируют любой нестандартный и уникальный процесс.</p>
+					<p>Местом хранения данных может быть на выбор - пользовательский локальный компьютер, корпоративный сервер или облако cloud.dbco.ru. В последнем случае, получить результат можно без специальных навыков и помощи ИТ-специалистов.</p>
+				</div>
+				
+				<div class="col-6 d-none d-lg-block offer5_img_block">
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
 	
 	<div class="mt-5" style="text-align:center;">
 		<h1>Новинки</h1>
